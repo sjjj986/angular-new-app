103cfaca469d2ca0f70b9a0f9c551caded9bd62c
sjjj986 <sijinli@google.com>
2024-01-25 16:43:47 -0500

    create new angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  imports: [CommonModule], 
+  providers: [CommonModule]
 })
 export class AppComponent {
-  title = 'angular-new-app';
+  firestore: Firestore = inject(Firestore)
+  items$: Observable<any[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items$ = collectionData(aCollection);
+  }
 }
