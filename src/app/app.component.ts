7c32a53b75f06a69916834e9f2a45e591356d459
sjjj986 <sijinli@google.com>
2024-01-29 03:19:18 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,14 @@ import { CommonModule } from '@angular/common';
   providers: [CommonModule]
 })
 export class AppComponent {
-  firestore: Firestore = inject(Firestore)
-  items$: Observable<any[]>;
-
-  constructor() {
-    const aCollection = collection(this.firestore, 'items')
-    this.items$ = collectionData(aCollection);
+  title(title: any) {
+    throw new Error('Method not implemented.');
   }
+  // firestore: Firestore = inject(Firestore)
+  // items$: Observable<any[]>;
+
+  // constructor() {
+  //   const aCollection = collection(this.firestore, 'items')
+  //   this.items$ = collectionData(aCollection);
+  // }
 }
